5473437246776facd9b4ee8828773c8f7d461f2c
Sebastián Serrisuela <sebaserri@gmail.com>
2024-09-04 13:12:00 -0300

    Creo el PlacesService para obtener la geolocalización del dispositivo.

--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-map-screen',
   templateUrl: './map-screen.component.html',
   styleUrl: './map-screen.component.css'
 })
-export class MapScreenComponent {
+export class MapScreenComponent implements OnInit {
+
+  constructor( private placesService: PlacesService ) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+if ( !navigator.geolocation ) {
+  alert('Navegador no soporta la geolocalización.');
+  throw new Error('Navegador no soporta la geolocalización.');
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
 })

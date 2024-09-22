cf8335dd1b6c111ef48a199b6722ff8bf00504f4
Sebastián Serrisuela <sebaserri@gmail.com>
2024-09-22 18:27:48 -0300

    Obtenemos el PlacesResponse desde Mapbox.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapsModule } from './maps/maps.module';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { MapsModule } from './maps/maps.module';
     AppRoutingModule,
     MapsModule
   ],
-  providers: [],
+  providers: [ provideHttpClient() ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature, PlacesResponse } from '../interfaces/places';
 
 @Injectable({
   providedIn: 'root'
@@ -6,12 +8,14 @@ import { Injectable } from '@angular/core';
 export class PlacesService {
 
   public userLocation?: [number, number];
+  public isLoadingPlaces: boolean = false;
+  public places: Feature[] = []
 
   get isUserLocationReady(): boolean{
     return !!this.userLocation;
   }
 
-  constructor() {
+  constructor( private http: HttpClient ) {
     this.getUserLocation();
   }
 
@@ -32,6 +36,21 @@ export class PlacesService {
         );
 
       });
+    }
+
+    public getPlacesByQuery( query: string ) {
+      //todo: evaluar cuando el query es un string vacio
+
+      this.isLoadingPlaces = true;
+
+      this.http.get<PlacesResponse> (`https://api.mapbox.com/search/geocode/v6/forward?q=${ query }&limit=5&proximity=-65.19821101549233,2C-26.824601637804818&language=es&access_token=pk.eyJ1IjoiZWxmbGFjb3NlYmEiLCJhIjoiY2x2dHQ5ZXVmMWVpNzJqcGd3ZGxxczdnNCJ9.fPfAV9MvOJ4PoSeOytNHKg`)
+        .subscribe( resp => {
+          console.log( resp.features );
+
+          this.isLoadingPlaces = false;
+          this.places = resp.features;
+
+        });
     }
 
 }

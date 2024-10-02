6e010f1a9d4155f3b1793627c23a10e51ed9cf9d
Sebastián Serrisuela <sebaserri@gmail.com>
2024-10-02 17:02:10 -0300

    Ajusto el mapa con todos los marcadores

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -59,7 +59,7 @@ export class PlacesService {
           this.isLoadingPlaces = false;
           this.places = resp.features;
 
-          this.mapService.createMarkersFromPlaces( this.places );
+          this.mapService.createMarkersFromPlaces( this.places, this.userLocation! );
 
         });
     }
